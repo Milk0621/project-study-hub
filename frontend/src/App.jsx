a4aee9b51ebfc13524b65ff330511ecb7acddc3c
Milk0621 <wltjs5360@gmail.com>
2025-07-07 18:01:20 +0900

    feat: 다크/라이트 모드 전환 스위치 구현 및 스타일 커스터마이징

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true); //기본 다크모드
   return (
-    <div className="App">
-      
+    <div className={darkMode ? "App dark" : "App light"}>
+      <Form>
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          label={darkMode ? "Dark" : "Light"}
+          onClick={()=>{
+            setDarkMode(!darkMode)
+          }}
+        />
+      </Form>
     </div>
   );
 }
