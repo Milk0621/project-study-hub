613af8cef7f865f09c176dd45257b36bd0f126bc
Milk0621 <wltjs5360@gmail.com>
2025-07-07 19:02:37 +0900

    feat: 네비게이션 메뉴 수정 및 로그인 버튼 추가

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import { Form, Navbar, Nav, Container } from 'react-bootstrap';
 
 function Main(){
@@ -7,26 +6,27 @@ function Main(){
     const [darkMode, setDarkMode] = useState(true); //기본 다크모드
     return (
         <div className={darkMode ? "App dark" : "App light"}>
-            <Navbar variant={darkMode ? "dark" : "light"}>
+            <Navbar variant={darkMode ? "dark" : "light"} style={{borderBottom: '1px solid #35393d'}}>
                 <Container>
-                <Navbar.Brand>Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link>Home</Nav.Link>
-                    <Nav.Link>Features</Nav.Link>
-                    <Nav.Link>Pricing</Nav.Link>
-                </Nav>
-                <Nav>
-                    <Form>
-                    <Form.Check
-                        type="switch"
-                        id="custom-switch"
-                        label={darkMode ? "Dark" : "Light"}
-                        onClick={()=>{
-                        setDarkMode(!darkMode)
-                        }}
-                    />
-                    </Form>
-                </Nav>
+                    <Navbar.Brand>Study Sync</Navbar.Brand>
+                    <Nav className="me-auto" style={{gap:'10px'}}>
+                        <Nav.Link>홈</Nav.Link>
+                        <Nav.Link>내 그룹</Nav.Link>
+                        <Nav.Link>마이페이지</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Form>
+                        <Form.Check
+                            type="switch"
+                            id="custom-switch"
+                            label={darkMode ? "Dark" : "Light"}
+                            onClick={()=>{
+                            setDarkMode(!darkMode)
+                            }}
+                        />
+                        </Form>
+                    </Nav>
+                    <button id="login-btn">로그인</button>
                 </Container>
             </Navbar>
         </div>
