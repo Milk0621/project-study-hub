1046fdff5c76c75ad89c72373f0bdad0ffa8c63d
Milk0621 <wltjs5360@gmail.com>
2025-07-07 23:22:44 +0900

    feat: 시:분:초 형식의 타이머 기능 구현

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Form, Navbar, Nav, Container } from 'react-bootstrap';
 
 function Main(){
-  //start 누르면 시간 증가, stop 누르면 멈춤  
+  //start 누르면 시간 증가, stop 누르면 멈춤
+  const [time, setTime] = useState(0);
+  const intervalRef = useRef(null);
+  const [running, setRunning] = useState(false);
+
+  useEffect(()=>{
+    if(running){
+      intervalRef.current = setInterval(()=>{
+        setTime(prev => prev + 1);
+      }, 1000)
+    }else {
+      clearInterval(intervalRef.current); // UX 및 안정성 향상
+    }
+
+    return ()=>{
+      clearInterval(intervalRef.current); //cleanup
+    }
+  }, [running])
+
+  //초를 시:분:초로 변경하는 함수
+  function formatTime(seconds){
+    const hours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+    const min = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+    const sec = String(seconds % 60).padStart(2, '0');
+    return `${hours}:${min}:${sec}`
+  }
+
   return (
-      <span>00:00:00</span>
+    <div>
+      <span>{formatTime(time)}</span>
+      <button onClick={()=>{setRunning(true)}}>Start</button>
+      <button onClick={()=>{setRunning(false)}}>Stop</button>
+    </div>
   )
 }
 
