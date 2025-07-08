7f2a38fa722606a9b0f3c06e1c4a3afaaa481847
Milk0621 <wltjs5360@gmail.com>
2025-07-08 18:08:03 +0900

    feat: 로그인 시 아이디와 비밀번호 빈값 입력 검사 로직 추가

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -2,6 +2,19 @@ import { useState } from 'react';
 import styles from './Modal.module.css';
 function Modal({setModalOpen}){
     const [signup, setSignup] = useState(false);
+    const [userId, setUserId] = useState('');
+    const [userPw, setUserPw] = useState('');
+
+    const handleLogin = function(){
+        if(!userId || !userPw){
+            alert('아이디와 비밀번호를 입력해주세요.');
+            return;
+        }else{
+            alert('성공!');
+            return;
+        }
+    }
+
     return(
         <div className={styles.modalBg} onClick={() => setModalOpen(false)}>
             <div className={styles.modal} onClick={(e)=>e.stopPropagation()}>
@@ -17,10 +30,20 @@ function Modal({setModalOpen}){
                     </>
                 ) : (
                     <>
-                        <input type="text" placeholder="아이디" />
-                        <input type="password" placeholder="비밀번호" />
+                        <input 
+                            type="text"
+                            placeholder="아이디"
+                            value={userId}
+                            onChange={(e)=>setUserId(e.target.value)}
+                        />
+                        <input 
+                            type="password"
+                            placeholder="비밀번호" 
+                            value={userPw} 
+                            onChange={(e)=>setUserPw(e.target.value)}
+                        />
                         <p onClick={() => setSignup(true)}>회원가입</p>
-                        <button>로그인</button>
+                        <button onClick={() => handleLogin()}>로그인</button>
                     </>
                 )}
             </div>
