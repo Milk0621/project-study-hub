aef3e0ef02479eea8618f7e141e40bbddb23aed1
Milk0621 <wltjs5360@gmail.com>
2025-07-09 18:46:34 +0900

    feat: 전역 로그인 상태 유지를 위해 App 컴포넌트에 AuthProvider 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,31 @@
 import './App.css';
 import { createContext, useEffect, useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from './components/Main/Main'
-import Header from './components/Header/Header'
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
+import AuthProvider from './context/AuthContext';
 
 export const DarkModeContext = createContext();
 
 function App() {
+
   const [darkMode, setDarkMode] = useState(true); //기본 다크모드
+
   useEffect(() => {
     document.body.className = darkMode ? 'dark' : '';
   }, [darkMode]);
+
   return (
     <div className={darkMode ? "App dark" : "App light"}>
-      <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
-        <Header />
-      </DarkModeContext.Provider>
-      <Routes>
-        <Route path='/' element={ <Main /> }/>
-      </Routes>
+      <AuthProvider>
+        <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+          <Header />
+        </DarkModeContext.Provider>
+        <Routes>
+          <Route path='/' element={ <Main /> }/>
+        </Routes>   
+      </AuthProvider>
     </div>
   );
 }
