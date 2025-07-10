cccfd15f022a886bab90302e6e8f5e69cf5a034e
Milk0621 <wltjs5360@gmail.com>
2025-07-10 22:11:31 +0900

    refactor: AuthProvider를 default export에서 named export로 변경

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import api from "../api/axios";
 
 export const AuthContext = createContext();
 
-export default function AuthProvider({ children }){
+export function AuthProvider({ children }){
     const [user, setUser] = useState(null);
 
     useEffect(() => {
