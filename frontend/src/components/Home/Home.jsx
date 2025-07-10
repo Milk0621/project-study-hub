3f0239a6c73d7e7175b60c32c247f1a86bb4ee17
Milk0621 <wltjs5360@gmail.com>
2025-07-10 19:49:50 +0900

    chore: Home 화면에서 사용자 닉네임 출력 확인용 코드 추가

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,12 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { Form, Navbar, Nav, Container } from 'react-bootstrap';
 import Timer from '../Timer/Timer'
+import { AuthContext } from '../../context/AuthContext';
 
 function Home(){
 
+  const {user} = useContext(AuthContext);
+  console.log(user);
+  
   return (
     <>
       <Timer />
+      {user?.nickname}
     </>
   );
 }
