45fab86d2978761c29404d05963960a627690220
Milk0621 <wltjs5360@gmail.com>
2025-07-11 14:40:18 +0900

    feat: 버튼에 /groupCreate 연결

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,8 +3,10 @@ import Timer from '../Timer/Timer'
 import { useSelector } from 'react-redux';
 import style from './Home.module.css';
 import GroupList from '../GroupList/GroupList';
+import { useNavigate } from 'react-router-dom';
 
 function Home(){
+  const navigate = useNavigate();
 
   const user = useSelector((state) => state.user.user);
   
@@ -14,7 +16,7 @@ function Home(){
       <div className={style.groupList}>
         <div className={style.groupListTop}>
           <h4 style={{margin: '0'}}>그룹 목록</h4>
-          <button> + 그룹 만들기</button>
+          <button onClick={()=>navigate('/groupCreate')}> + 그룹 만들기</button>
         </div>
         <GroupList />
       </div>
