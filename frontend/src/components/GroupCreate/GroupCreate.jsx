27d8576436c6fefaa2f039615dcd6e43fb36e97d
Milk0621 <wltjs5360@gmail.com>
2025-07-11 16:40:25 +0900

    feat: 그룹 생성 페이지 UI 레이아웃 및 기본 스타일링 구현

---
+++ frontend/src/components/GroupCreate/GroupCreate.jsx
@@ -0,0 +1,35 @@
+import style from './GroupCreate.module.css';
+import { useRef } from 'react';
+
+function GroupCreate() {
+
+    return(
+        <div className={style.wrap}>
+            <div className={style.groupCreateTop}>
+                <h4 style={{margin: '0'}}>그룹 만들기</h4>
+                <button>작성</button>
+            </div>
+            <div className={style.groupCreate}>
+                <input type="text" placeholder='그룹명' className={style.title} />
+                <input type="password" placeholder='비밀번호' />
+                <div className={style.radioBox}>
+                    <input type="radio" name="tag" id="radio1" />
+                    <label htmlFor="radio1">취업</label>
+                    <input type="radio" name="tag" id="radio2" />
+                    <label htmlFor="radio2">학업</label>
+                    <input type="radio" name="tag" id="radio3" />
+                    <label htmlFor="radio3">게임</label>
+                </div>
+                <textarea 
+                    name="content"
+                    className={style.content}
+                    placeholder="그룹 소개를 입력하세요."
+                    rows="20" 
+                    style={{resize: 'none'}}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default GroupCreate;
