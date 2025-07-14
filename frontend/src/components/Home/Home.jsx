e83b810d2465b39f344353ca472a290a2d1381df
Milk0621 <wltjs5360@gmail.com>
2025-07-14 12:50:56 +0900

    feat: 홈 화면에서 '그룹 만들기' 버튼 클릭 시 로그인 모달 표시 기능 추가

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,22 +1,32 @@
 import { useContext } from 'react';
 import Timer from '../Timer/Timer'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import style from './Home.module.css';
 import GroupList from '../GroupList/GroupList';
 import { useNavigate } from 'react-router-dom';
+import { openModal } from '../../store/modalSlice';
 
 function Home(){
   const navigate = useNavigate();
 
   const user = useSelector((state) => state.user.user);
-  
+
+  const dispatch = useDispatch();
+  function loginCheck(){
+    if(!user){
+      dispatch(openModal());
+    }else {
+      navigate('/groupCreate');
+    }
+  }
+
   return (
     <>
       <Timer />
       <div className={style.wrap}>
         <div className={style.groupListTop}>
           <h4 style={{margin: '0'}}>그룹 목록</h4>
-          <button onClick={()=>navigate('/groupCreate')}> + 그룹 만들기</button>
+          <button onClick={()=>loginCheck()}> + 그룹 만들기</button>
         </div>
         <GroupList />
       </div>
