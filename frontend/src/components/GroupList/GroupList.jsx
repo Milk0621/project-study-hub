51cf1b63c0bf27bd697cd4613ce784303705a748
Milk0621 <wltjs5360@gmail.com>
2025-07-14 15:36:19 +0900

    feat: 그룹 목록 조회 및 화면 렌더링 구현 (GroupList에서 map으로 출력)

--- frontend/src/components/GroupList/GroupList.jsx
+++ frontend/src/components/GroupList/GroupList.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from 'react';
 import style from './GroupList.module.css';
+import api from '../../api/api';
 
 function GroupList(props){
+
+  const [groupList, setGroupList] = useState([]);
+
+  useEffect(()=>{
+    const fetchGroups = async () => {
+      const res = await api.get('/groups/list');
+      setGroupList(res.data);
+      console.log(res.data);
+    }
+    fetchGroups();
+  }, [])
+
   return(
-    <div className={style.groupListBg}>
-      <span className={style.groupInfo}> 홍길동 · 조회 90 · 2025-07-11</span>
-      ★☆
-      <h5 style={{fontWeight: '600'}}>취준 스터디원 모집합니다 (온라인)</h5>
-      <p>스터디 소개 꾸준한 루틴과 집중력을 함께 지켜가며, 각자 취업 준비를 성실하게 이어가는 스터디입니다. 함께 모여 서로에게 자극을 주고 받으며 취업에 골인해보아요! 성실히! 열심히! 꾸준히! 하기로 약속~~</p>
-      <span className={style.hashTag}>개발자</span><span className={style.hashTag}>취업</span>
-    </div>
+    <>
+      { groupList.map((group)=>(
+        <div key={group.id} className={style.groupListBg}>
+          <span className={style.groupInfo}> {group.createUser} · 조회 {group.hit} · {group.createDate} </span>
+          ★☆
+          <h5 style={{fontWeight: '600'}}> {group.groupName} </h5>
+          <p> {group.content} </p>
+          <span className={style.hashTag}>{group.tag}</span>
+        </div>
+      ))}
+    </>
   )
 }
 
