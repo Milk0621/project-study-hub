6d0f6d6193aaa2dd958dc5f43f8318d128b2c225
Milk0621 <wltjs5360@gmail.com>
2025-07-14 17:01:45 +0900

    feat: 그룹 단건 조회를 위한 컨트롤러 및 MyBatis 매퍼 구현

--- backend/src/main/java/com/studysync/backend/domain/groups/controller/GroupsController.java
+++ backend/src/main/java/com/studysync/backend/domain/groups/controller/GroupsController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.studysync.backend.domain.groups.model.Groups;
@@ -34,5 +36,12 @@ public class GroupsController {
 	@GetMapping("/list")
 	public ResponseEntity<?> getGroupList(){
 		return ResponseEntity.ok(groupsService.getGroupList());
+	}
+	
+	@GetMapping("/post/{id}")
+	public ResponseEntity<?> getGroupById(@PathVariable int id) {
+		//@RequestParam는 쿼리 파라미터 형식에 사용 /post?id=1
+		//@PathVariable은 경로 파라미터 형식에 사용 /post/1
+		return ResponseEntity.ok(groupsService.getGroupById(id));
 	}
 }
