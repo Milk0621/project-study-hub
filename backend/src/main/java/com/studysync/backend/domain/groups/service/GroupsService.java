08a8f7ca2f9098921079e0b5597432f2ba26b638
Milk0621 <wltjs5360@gmail.com>
2025-07-14 14:58:05 +0900

    feat: GroupsService와 GroupsServiceImpl에 그룹 전체 조회 메서드 getGroupList 구현

--- backend/src/main/java/com/studysync/backend/domain/groups/service/GroupsService.java
+++ backend/src/main/java/com/studysync/backend/domain/groups/service/GroupsService.java
@@ -1,9 +1,12 @@
 package com.studysync.backend.domain.groups.service;
 
+import java.util.List;
+
 import com.studysync.backend.domain.groups.model.Groups;
 
 public interface GroupsService {
 	//그룹 생성
 	int registerGroup(Groups groups);
-	
+	//그룹 전체 조회
+	List<Groups> getGroupList();
 }
--- backend/src/main/java/com/studysync/backend/domain/groups/service/GroupsServiceImpl.java
+++ backend/src/main/java/com/studysync/backend/domain/groups/service/GroupsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.studysync.backend.domain.groups.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,11 @@ public class GroupsServiceImpl implements GroupsService{
 	@Override
 	public int registerGroup(Groups groups) {
 		return groupsDAO.insertGroup(groups);
+	}
+
+	@Override
+	public List<Groups> getGroupList() {
+		return groupsDAO.selectAllGroups();
 	}
 	
 }
