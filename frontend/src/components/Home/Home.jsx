f645d3a6075b2ad72f5b2c927863469779b3df65
Milk0621 <wltjs5360@gmail.com>
2025-07-15 16:42:51 +0900

    refactor: Home에서 그룹 목록 API 호출하도록 로직 분리

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,17 +1,28 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Timer from '../Timer/Timer'
 import { useDispatch, useSelector } from 'react-redux';
 import style from './Home.module.css';
 import GroupList from '../GroupList/GroupList';
 import { useNavigate } from 'react-router-dom';
 import { openModal } from '../../store/modalSlice';
+import api from '../../api/api';
 
 function Home(){
   const navigate = useNavigate();
-
   const user = useSelector((state) => state.user.user);
-
   const dispatch = useDispatch();
+
+  const [groups, setGroups] = useState([]);
+  
+  useEffect(()=>{
+    const fetchGroups = async () => {
+      const res = await api.get('/groups/list');
+      setGroups(res.data);
+      console.log(res.data);
+    }
+    fetchGroups();
+  }, [])
+
   function loginCheck(){
     if(!user){
       dispatch(openModal());
@@ -26,9 +37,13 @@ function Home(){
       <div className={style.wrap}>
         <div className={style.groupListTop}>
           <h4 style={{margin: '0'}}>그룹 목록</h4>
-          <button onClick={()=>loginCheck()}> + 그룹 만들기</button>
+          {/* <button onClick={()=>loginCheck()}> + 그룹 만들기</button> */}
+          <div>
+            <input type="text" placeholder='검색어를 입력하세요.'/>
+            <button>검색</button>
+          </div>
         </div>
-        <GroupList />
+        <GroupList groups={groups} />
       </div>
     </>
   );
