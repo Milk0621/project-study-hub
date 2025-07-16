b23021eeef54af57b5b64240e1d64392e46a0f76
Milk0621 <wltjs5360@gmail.com>
2025-07-16 15:36:50 +0900

    feat: 그룹멤버를 관리하기 위한 모델 생성

---
+++ backend/src/main/java/com/studysync/backend/domain/groupmembers/model/GroupMembers.java
@@ -0,0 +1,38 @@
+package com.studysync.backend.domain.groupmembers.model;
+
+import java.time.LocalDateTime;
+
+public class GroupMembers {
+	private int groupId;
+	private String userId;
+	private LocalDateTime joinedAt;
+	
+	public GroupMembers() {}
+	
+	public GroupMembers(int groupId, String userId, LocalDateTime joinedAt) {
+		this.groupId = groupId;
+		this.userId = userId;
+		this.joinedAt = joinedAt;
+	}
+
+	public int getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public LocalDateTime getJoinedAt() {
+		return joinedAt;
+	}
+	public void setJoinedAt(LocalDateTime joinedAt) {
+		this.joinedAt = joinedAt;
+	}
+	
+	
+}
