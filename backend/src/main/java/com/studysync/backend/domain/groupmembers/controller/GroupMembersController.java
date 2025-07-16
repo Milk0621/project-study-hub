651b741f98dc3d1a7b8b79a9c07eee38a137a906
Milk0621 <wltjs5360@gmail.com>
2025-07-16 18:01:23 +0900

    feat: 그룹 참여 여부 확인을 위한 컨트롤러 엔드포인트 추가

--- backend/src/main/java/com/studysync/backend/domain/groupmembers/controller/GroupMembersController.java
+++ backend/src/main/java/com/studysync/backend/domain/groupmembers/controller/GroupMembersController.java
@@ -3,9 +3,11 @@ package com.studysync.backend.domain.groupmembers.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.studysync.backend.domain.groupmembers.model.GroupMembers;
@@ -28,5 +30,15 @@ public class GroupMembersController {
 		return result > 0
 				? ResponseEntity.ok("그룹 참여 완료")
 				: ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("그룹 참여 실패");
+	}
+	
+	@GetMapping("/checkJoin")
+	public ResponseEntity<?> checkJoin(@RequestParam int groupId, @RequestParam String userId){
+		GroupMembers result = groupMemberService.checkJoin(groupId, userId);
+		if(result != null) {
+			return ResponseEntity.ok("그룹 참여 되어있음");
+		}else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("그룹에 참여되어 있지 않음");
+		}
 	}
 }
