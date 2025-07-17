a078c150c61523b7d1a10553727cdcef939cb3c1
Milk0621 <wltjs5360@gmail.com>
2025-07-17 17:49:22 +0900

    refactor: 참여 그룹 전체 조회 후 프론트에서 내가 만든 그룹과 참여 그룹 분리

--- frontend/src/components/MyGroup/MyGroup.jsx
+++ frontend/src/components/MyGroup/MyGroup.jsx
@@ -12,25 +12,28 @@ function MyGroup() {
     if (!user) return;
 
     const fetchMyGroups = async () => {
-      const res = await api.get(`/groups/my?userId=${user.id}`);
-      setMyGroups(res.data);
-    };
-
-    const fetchJoinedGroups = async () => {
-      const res = await api.get(`/groups/joined?userId=${user.id}`);
-      setJoinedGroups(res.data);
+      const res = await api.get(`/groups/my`, {
+        params: {userId: user.id}
+      });
+      const allGroups = res.data;
+      
+      //내가 만든 그룹
+      const myGroups = allGroups.filter(group => group.createUser === user.id);
+      //내가 참여중인 그룹
+      const joinedGroups = allGroups.filter(group => group.createUser !== user.id);
+      setMyGroups(myGroups);
+      setJoinedGroups(joinedGroups);
     };
 
     fetchMyGroups();
-    fetchJoinedGroups();
   }, [user]);
 
   return (
-    <div>
-      <h2>내가 만든 그룹</h2>
+    <div className='wrap'>
+      <h4>내가 만든 그룹</h4>
       <GroupList groups={myGroups} />
 
-      <h2>참여 중인 그룹</h2>
+      <h4>참여 중인 그룹</h4>
       <GroupList groups={joinedGroups} />
     </div>
   );
