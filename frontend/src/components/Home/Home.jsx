e037e9df5ab6e70f4c5c5637829b375c028e0760
Milk0621 <wltjs5360@gmail.com>
2025-07-17 20:29:01 +0900

    feat: 그룹 만들기 버튼을 Home에서 MyGroup으로 이동하고 관련 CSS 수정

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,13 +4,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import style from './Home.module.css';
 import GroupList from '../GroupList/GroupList';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { openModal } from '../../store/modalSlice';
 import api from '../../api/api';
 
 function Home(){
   const navigate = useNavigate();
   const user = useSelector((state) => state.user.user);
-  const dispatch = useDispatch();
 
   const [search, setSearch] = useState('');
   const [select, setSelect] = useState('');
@@ -61,21 +59,12 @@ function Home(){
     fetchGroups();
   }, [])
 
-  function loginCheck(){
-    if(!user){
-      dispatch(openModal());
-    }else {
-      navigate('/groupCreate');
-    }
-  }
-
   return (
     <>
       <Timer />
       <div className="wrap">
         <div className={style.groupListTop}>
           <h4 style={{margin: '0'}}>그룹 목록</h4>
-          <button onClick={()=>loginCheck()}> + 그룹 만들기</button>
           <div>
             <select value={select} onChange={(e)=>handleSelectChange(e)}>
               <option value="">전체</option>
--- frontend/src/components/MyGroup/MyGroup.jsx
+++ frontend/src/components/MyGroup/MyGroup.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react';
 import GroupList from '../GroupList/GroupList';
 import api from '../../api/api';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { openModal } from '../../store/modalSlice';
+import style from './MyGroup.module.css';
 
 function MyGroup() {
   const user = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [myGroups, setMyGroups] = useState([]);
   const [joinedGroups, setJoinedGroups] = useState([]);
 
@@ -28,9 +33,20 @@ function MyGroup() {
     fetchMyGroups();
   }, [user]);
 
+  function loginCheck(){
+    if(!user){
+      dispatch(openModal());
+    }else {
+      navigate('/groupCreate');
+    }
+  }
+
   return (
     <div className='wrap'>
-      <h4>내가 만든 그룹</h4>
+      <div className={style.myGroupTop}>
+        <h4>내가 만든 그룹</h4>
+        <button onClick={()=>loginCheck()}> + 그룹 만들기</button>
+      </div>
       <GroupList groups={myGroups} />
 
       <h4>참여 중인 그룹</h4>
