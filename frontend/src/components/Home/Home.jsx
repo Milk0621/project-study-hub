54f30c7043867f1e001c71537ef2430f4d2c509f
Milk0621 <wltjs5360@gmail.com>
2025-07-17 14:47:44 +0900

    feat: 그룹 목록 필터링을 위한 카테고리 선택 기능 추가

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import Timer from '../Timer/Timer'
 import { useDispatch, useSelector } from 'react-redux';
 import style from './Home.module.css';
 import GroupList from '../GroupList/GroupList';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { openModal } from '../../store/modalSlice';
 import api from '../../api/api';
 
@@ -11,6 +11,22 @@ function Home(){
   const navigate = useNavigate();
   const user = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
+
+  const [select, setSelect] = useState('');
+  const [searchParams] = useSearchParams();
+  useEffect(()=>{
+    const category = searchParams.get("category")
+    if(category) setSelect(category);
+  }, [searchParams]);
+  const handleSelectChange = (e) => {
+    const value = e.target.value;
+    setSelect(value);
+    if(value){
+      navigate(`/?category=${value}`);
+    } else {
+      navigate(`/`);
+    }
+  }
 
   const [groups, setGroups] = useState([]);
   
@@ -37,13 +53,22 @@ function Home(){
       <div className={style.wrap}>
         <div className={style.groupListTop}>
           <h4 style={{margin: '0'}}>그룹 목록</h4>
-          {/* <button onClick={()=>loginCheck()}> + 그룹 만들기</button> */}
+          <button onClick={()=>loginCheck()}> + 그룹 만들기</button>
           <div>
+            <select value={select} onChange={(e)=>handleSelectChange(e)}>
+              <option value="">전체</option>
+              <option value="취업">취업</option>
+              <option value="학업">학업</option>
+              <option value="자격증">자격증</option>
+              <option value="운동">운동</option>
+              <option value="게임">게임</option>
+              <option value="기타">기타</option>
+            </select>
             <input type="text" placeholder='검색어를 입력하세요.'/>
             <button>검색</button>
           </div>
         </div>
-        <GroupList groups={groups} />
+        <GroupList groups={groups} category={select}/>
       </div>
     </>
   );
