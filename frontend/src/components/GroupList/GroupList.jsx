e68615cbe84dc93ce1517e54e78a28108f264115
Milk0621 <wltjs5360@gmail.com>
2025-07-21 11:55:08 +0900

    feat: 그룹 리스트 컴포넌트에서 즐겨찾기 ID 목록 조회 및 상태 저장 로직 추가

--- frontend/src/components/GroupList/GroupList.jsx
+++ frontend/src/components/GroupList/GroupList.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
 import style from './GroupList.module.css';
 import api from '../../api/api';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { addScrap, removeScrap } from '../../store/scrapSlice';
-import axios from 'axios';
+import { addScrap, removeScrap, setScrapList } from '../../store/scrapSlice';
+import { useEffect } from 'react';
 
 function GroupList({groups, category}){
+  const user = useSelector((state)=>state.user.user);
   const navigate = useNavigate();
   const scrapList = useSelector((state)=>state.scrap.scrapList);
   const dispatch = useDispatch();
@@ -15,10 +15,6 @@ function GroupList({groups, category}){
     e.stopPropagation();
 
     const token = localStorage.getItem("token");
-    if (!token) {
-      alert("로그인이 필요합니다.");
-      return;
-    }
 
     if (scrapList.includes(groupId)) {
       await api.delete(`/groupScrap/${groupId}`, {
@@ -32,6 +28,20 @@ function GroupList({groups, category}){
       dispatch(addScrap(groupId));
     }
   }
+
+  useEffect(()=>{
+    const fetchScraps = async () => {
+      try{
+        const res = await api.get('/groupScrap/scrapId');
+        console.log(res.data);
+        const scrapId = res.data;
+        dispatch(setScrapList(scrapId));
+      }catch (err){
+        console.log(err);
+      }
+    }
+    fetchScraps();
+  }, [])
 
   return(
     <>
@@ -50,9 +60,11 @@ function GroupList({groups, category}){
             }}
           >
             <span className={style.groupInfo}> {group.createUser} · 조회 {group.hit} · {group.createDate} </span>
-            <button className={style.scrapBtn} onClick={(e)=>toggleScrap(e, group.id)}>
-              {scrapped ? "★" : "☆"}
-            </button>
+            {user && (
+              <button className={style.scrapBtn} onClick={(e)=>toggleScrap(e, group.id)}>
+                {scrapped ? "★" : "☆"}
+              </button>
+            )}
             <h5 style={{fontWeight: '600'}}> {group.groupName} </h5>
             <p className={`${style.contentLineClamp}`}> {group.content} </p>
             <span className={style.hashTag}>{group.tag}</span>
