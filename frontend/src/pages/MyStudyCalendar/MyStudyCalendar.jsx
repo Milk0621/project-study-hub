2a5d3e5376ab77e5eb669e821e102a0b3cb6046c
Milk0621 <wltjs5360@gmail.com>
2025-07-24 14:02:34 +0900

    feat: 마이페이지 캘린더에 날짜별 공부 시간 표시 기능 추가

--- frontend/src/pages/MyStudyCalendar/MyStudyCalendar.jsx
+++ frontend/src/pages/MyStudyCalendar/MyStudyCalendar.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import StudyCalendar from "../../components/StudyCalendar/StudyCalendar";
 import api from "../../api/api";
+import { useSelector } from "react-redux";
 
 function MyStudyCalendar(){
+    const user = useSelector((state)=>state.user.user);
     const [date, setDate] = useState(new Date());
     const [studyRecords, setStudyRecords] = useState({});
 
@@ -23,13 +25,42 @@ function MyStudyCalendar(){
         return 'calendar-color0';
     };
 
+    const formatDate = (date) => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    };
+
+    const formatSeconds = (seconds) => {
+        const h = Math.floor(seconds / 3600);
+        const m = Math.floor((seconds % 3600) / 60);
+        const s = seconds % 60;
+
+        const pad = (num) => String(num).padStart(2, '0');
+        return `${pad(h)}시간 ${pad(m)}분 ${pad(s)}초`;
+    };
+
     return(
-        <StudyCalendar 
-            studyRecords={studyRecords}
-            date={date}
-            setDate={setDate}
-            tileColorResolver={tileColorResolver}
-        />
+        <div className="wrap">
+            <StudyCalendar 
+                studyRecords={studyRecords}
+                date={date}
+                setDate={setDate}
+                tileColorResolver={tileColorResolver}
+            />
+            <div style={{padding: '20px 0'}}>
+                <span>{formatDate(date)}</span>
+                <h4>{user.nickname}님의 공부 시간</h4>
+                {
+                    studyRecords[formatDate(date)] !== undefined ? (
+                        <p>{formatSeconds(studyRecords[formatDate(date)])}</p>
+                    ) : (
+                        <p>기록이 없습니다.</p>
+                    )
+                }
+            </div>
+        </div>
     )
 }
 
